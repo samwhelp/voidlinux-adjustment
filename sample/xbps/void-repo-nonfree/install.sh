62d2061396f53c4a75301c483ca13a7410d10776
samwhelp <samwhelp@users.noreply.github.com>
2025-12-12 23:21:25 +0000

    fix

--- sample/xbps/void-repo-nonfree/install.sh
+++ sample/xbps/void-repo-nonfree/install.sh
@@ -14,3 +14,8 @@ sudo xbps-install -S
 ## install sublime-text4
 sudo xbps-install -Su sublime-text4
 
+
+## add /usr/bin/subl symbolic link to /usr/bin/subl4
+cd /usr/bin
+sudo ln -sf subl4 subl
+file file /usr/bin/subl
